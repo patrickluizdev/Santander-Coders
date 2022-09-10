8bb3a9987589f1f950f54a668156ee6523662ab6
Patrick Luiz Duque de Paula Nascimento <patrickluizjf@hotmail.com>
2022-09-10 07:28:38 -0300

    Operadores numericos

---
+++ Hello World/src/Main.java
@@ -0,0 +1,49 @@
+public class Main {
+    public static void main(String[] args) {
+        //
+        //System.out.println("Hello world!");
+        //
+        String nome;
+        nome = "Let's Code";
+
+        System.out.println("Olá, " + nome);
+        ////////////////////////////////////////////////////////////////////////
+        System.out.println("Nessa aula conhecemos os tipos primitivos de dados, que são: ");
+        System.out.println("byte, short, int, long, float, double, boolean, char");
+        System.out.println("Por convenção para declarar valores numericos inteiros usa-se int, pois já possui um intervalo suficiente");
+        System.out.println("Para numeros com casas decimais usa-se o float ou double");
+        System.out.println("Para tipos logicos utilizamos boolean");
+        System.out.println("Para um unico caracter utilizamos char");
+        ////////////////////////////////////////////////////////////////////////
+        System.out.println("////////////////////////////////////////////////////////////////////////");
+        ////////////////////////////////////////////////////////////////////////
+        System.out.println("Exemplos a=3 & b=2");
+
+        int a;
+        int b;
+        //
+        b = 2;
+        a = 3;
+        //
+        int soma = a+b;
+        int subtracao = a - b;
+        int multiplicacao = a*b;
+        float divisao = (float) a/b;
+        //
+        System.out.println("Soma: " + soma);
+        System.out.println("Subtração: " +subtracao);
+        System.out.println("Multiplicação: "+multiplicacao);
+        System.out.println("Divisão: "+divisao);
+        System.out.println("////////////////////////////////////////////////////////////////////////");
+
+
+
+
+
+
+
+
+
+        //
+    }
+}
